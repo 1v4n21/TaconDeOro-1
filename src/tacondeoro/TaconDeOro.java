15c083f52b1d99caeeec737977078c8d0a2597cc
dawcarlosp <131007231+dawcarlosp@users.noreply.github.com>
2023-04-17 17:36:57 +0200

    TaconDeOro: Crear clase Ventana

--- src/tacondeoro/TaconDeOro.java
+++ src/tacondeoro/TaconDeOro.java
@@ -15,7 +15,8 @@ public class TaconDeOro {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-      
+      Ventana v = new Ventana();
+      v.setVisible(true);
                 
     }
     
